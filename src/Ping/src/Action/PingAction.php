89b9fa490aa6fb13db2280bd24de6477db54ca3a
Ian <ian@Clements-MacBook-Pro.local>
2019-03-08 11:10:37 +0800

    First commit Description: setup expressive app and a simple GET /ping API Commiter: Ian

---
+++ src/Ping/src/Action/PingAction.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ping\Action;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use Zend\Diactoros\Response\JsonResponse;
+
+class PingAction implements RequestHandlerInterface
+{
+    public function handle(ServerRequestInterface $request) : ResponseInterface
+    {
+        return new JsonResponse('Ping successfully at '. date('d-m-y'), 200);
+    }
+}
