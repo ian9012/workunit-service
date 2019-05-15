a6a443f0cdfde1e06766ea80c8375985c7c352cf
Ian <iancharles901223@gmail.com>
2019-05-15 23:24:44 +0800

    Update PingAction.php

--- src/Ping/src/Action/PingAction.php
+++ src/Ping/src/Action/PingAction.php
@@ -13,18 +13,22 @@ class PingAction implements RequestHandlerInterface
 {
     public function handle(ServerRequestInterface $request) : ResponseInterface
     {
-        $message = 'Ping successfully at '. date('d-m-y');
+        $message = 'Ping successfully at Modified '. date('d-m-y');
         return new JsonResponse($message, 200);
     }
     
     public function anotherhandle(ServerRequestInterface $request) : ResponseInterface
     {
-        $message = 'Ping 2 successfully at '. date('d-m-y');
-        return new JsonResponse($message, 200);
+        return new JsonResponse('Ping successfully at Modified ', 200);
     }
    
     public function anotherhandle2(ServerRequestInterface $request) : ResponseInterface
     {
         return new JsonResponse('Ping 3 successfully at '. date('d-m-y'), 200);
+    }
+   
+    public function anotherhandle3(ServerRequestInterface $request) : ResponseInterface
+    {
+        return new JsonResponse('Ping 4 successfully at '. date('d-m-y'), 200);
     }
 }
